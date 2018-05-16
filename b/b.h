ac0ef6b0dc39c6e26d9df0fd7825cb02cc935bf4
Bradley Walters <bwalters@anonyome.com>
2018-05-16 14:37:08 -0600

    b

---
+++ b/b.h
@@ -0,0 +1,18 @@
+//
+//  b.h
+//  b
+//
+//  Created by Bradley Walters on 5/16/18.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for b.
+FOUNDATION_EXPORT double bVersionNumber;
+
+//! Project version string for b.
+FOUNDATION_EXPORT const unsigned char bVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <b/PublicHeader.h>
+
+
---
+++ b/b.swift
@@ -0,0 +1,12 @@
+//
+//  b.swift
+//  b
+//
+//  Created by Bradley Walters on 5/16/18.
+//
+
+import Foundation
+
+func b() -> String {
+    return "b"
+}
